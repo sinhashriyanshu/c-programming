a78291e505dd10663643ec833f62ce97ffa9674a
Shriyanshu Sinha <shriyanshusinhapatna@gmail.com>
2024-08-16 19:03:09 +0530

    Update exponentialvalue.c

--- exponentialvalue.c
+++ exponentialvalue.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int a,i,b,prod;
+  printf("Enter the Base");
+  scanf("%d",&a);
+  printf("Enter the Power");
+  scanf("%d",&b);
+  prod=1;
+for(i=1;i<=b;i++)
+  prod = prod*a;
 
+printf("The Answer is %d",prod);
+}
